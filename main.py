2ec956ea3fc4bc3da6fbd34494cf95b6c8df7f8f
Shahab <shahabbng1999@gmail.com>
2020-08-12 19:32:03 +0500

    -main.py- 1) reshaped X from 3d to 2d matrix 2) added bias unit to X 3) initialised random weights

--- main.py
+++ main.py
@@ -4,21 +4,31 @@ import matplotlib.pyplot as plt #for plotting
 import idx2numpy 
 
 #import pandas as pd
-#import numpy as np
+import numpy as np
 
-#load the data(in idx format) to numpy's ndarray
-arr = idx2numpy.convert_from_file("E:\Courses\LocalRepo\Digit_Recogniser\\t10k-images-idx3-ubyte")
-
-
-print(len(arr))
-#plot the image using pyplot
-"""plt.imshow(arr[12])
-plt.show()
-"""
+# load the data(in idx format) to numpy's ndarray
+X3d = idx2numpy.convert_from_file("E:\Courses\LocalRepo\Digit_Recogniser\\t10k-images-idx3-ubyte")
+y   = idx2numpy.convert_from_file("E:\Courses\LocalRepo\Digit_Recogniser\\t10k-labels-idx1-ubyte")
 
 
+# # print(len(X3d))
+# #plot the image using pyplot
+# print(y[0])
+# plt.imshow(X3d[0])
+# plt.show()
 
+#reshape 3d matrix to 2d
+# e.g: [10, 3, 3] --> [10, 9]
+X = X3d.reshape(len(X3d), -1)
+# we've to add bias unit as always
+X = np.c_[np.ones(len(X)), X]
 
+#random weights for our feedforward propogation
+# 25 neurons in hidden layer + bias unit
+theta1 = np.random.rand(25, X.shape[1])
+# there will be 10 classes
+theta2 = np.random.rand(10, 26)
+print(theta1.shape)
 
 
 
