afc668f727f373ee41351a4e8abc81f50add1763
Shahab <shahabbng1999@gmail.com>
2020-08-13 15:41:45 +0500

    Implementd J(theta)

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ print(y.size)
 #reshape 3d matrix to 2d
 # e.g: [10, 3, 3] --> [10, 9]
 X = X3d.reshape(len(X3d), -1)
-
+m = len(X) # size of training set
 h = nnPy.Feedforward(X)
 
 #encoding each row of 'y' in logical array
@@ -34,9 +34,12 @@ one_hot = np.zeros((y.size, y.max()+1))
 rows = np.arange(y.size)
 one_hot[rows, y] = 1
 
-# f1 = np.matmul(y, (np.log(h)).transpose())
+f1 = -(np.multiply(   one_hot,  np.log(h)))
+f2 = -(np.multiply((1-one_hot), np.log(1-h)))
+J = (1/m)*np.sum(np.sum(f1 + f2))
 # print(one_hot.shape, h.shape)
 
+print(J)
 
 
 
